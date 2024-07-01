fae46c3e756096f446617e7950d266d30b80179c
Jeetbhuva1019 <162433565+Jeetbhuva1019@users.noreply.github.com>
2024-07-01 14:22:41 +0530

    day30

---
+++ day30/p1.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    $("#submit").submit(function (event) {
+        event.preventDefault()
+        $("#err").text("");
+
+        let username = $("#username").val()
+        let username_regex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/
+
+        if (username === "") {
+            $("#err").text("Enter a valid username.")
+        }
+        else {
+            if (!username_regex.test(username)) {
+                $("#err").text("Enter a valid username.")
+            }
+
+            let email = $("#email").val()
+            let email_regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+            if (email === "") {
+
+                $("#err").text("Enter a valid Email Id.")
+            }
+            else {
+                if (!email_regex.test(email)) {
+
+                    $("#err").text("Enter a valid Email Id.")
+                }
+            }
+
+            let password = $("#password").val()
+            let password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+            let confirm_password = $("#cpw").val()
+
+
+            if (password === "") {
+                $("#err").text("Enter a should have 1 uppercase charecter, 1 numeric, 1 special charecter.")
+            }
+            else {
+                if (!password_regex.test(password)) {
+                    $("#err").text("Enter a should have 1 uppercase charecter, 1 numeric, 1 special charecter.")
+
+                }
+            }
+
+            if (password !== confirm_password) {
+                $("#err").text("Password does not match.")
+            }
+        }
+    })
+
+})
